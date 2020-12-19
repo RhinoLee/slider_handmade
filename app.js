509f2c7d4d6fc663e76f97b89ed14db862900e20
RhinoLee <picklerhino@gmail.com>
2020-12-19 23:53:06 +0800

    add bullets

--- app.js
+++ app.js
@@ -1,5 +1,6 @@
 ;(function() {
  
+  const slider = document.querySelector('.slider-container')
   const prevBtn = document.querySelector('.slider-arrow > .prev')
   const nextBtn = document.querySelector('.slider-arrow > .next')
   const sliderItems = document.querySelectorAll('.slider-container .slide-item')
@@ -9,6 +10,41 @@
   let position = 0
 
   // activeIdx => 0 ~ 5 循環
+  const setBulletsTemplate = () => {
+    let bullet = ''
+    slider.insertAdjacentHTML('beforeend', '<div class="slide-bullets-bar"></div>')
+    
+    for(let i = 0; i < slideCount; i++){
+      bullet += '<div class="slide-bullet"></div>'
+    }
+
+    document.querySelector('.slide-bullets-bar').insertAdjacentHTML('beforeend', bullet)
+
+  }
+
+  const bulletHandler = () => {
+
+    const bullets = document.querySelectorAll('.slide-bullets-bar .slide-bullet')
+    bullets[activeIdx].classList.add('active')
+    bullets.forEach( (bullet, idx) => {
+
+      bullet.addEventListener('click', (e) =>{
+
+        bullets.forEach( (bullet, idx) => {
+          bullet.classList.remove('active')
+
+        })
+        e.currentTarget.classList.add('active')
+        setActiveIdx(idx)
+        setSlidePosition()
+
+      })
+    })
+
+    bullets[activeIdx].click()
+
+  }
+  
 
   const setActiveIdx = (num) => {
     switch(true){
@@ -49,28 +85,37 @@
 
     })
   }
+
+  
+  
   
 
   prevBtn.addEventListener('click', function(){
     activeIdx -= 1
     setActiveIdx(activeIdx)
+    bulletHandler()
     setSlidePosition()
   })
 
   nextBtn.addEventListener('click', function(){
     activeIdx += 1
     setActiveIdx(activeIdx)
+    bulletHandler()
     setSlidePosition()
   })
 
-  setSlidePosition()
 
+  
+  
+  
   window.addEventListener('resize', function(){
     windowWidth = window.innerWidth
     setSlidePosition()
   })
 
-
+  setBulletsTemplate()
+  setSlidePosition()
+  bulletHandler()
 
 
 })();
