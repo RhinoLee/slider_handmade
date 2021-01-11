555075a1f0084444a2415de260513fa7e44c63aa
RhinoLee <picklerhino@gmail.com>
2021-01-12 01:55:53 +0800

    prettier setSlidePosition

--- app.js
+++ app.js
@@ -3,6 +3,7 @@
   const slider = document.querySelector('.slider-container')
   const prevBtn = document.querySelector('.slider-arrow > .prev')
   const nextBtn = document.querySelector('.slider-arrow > .next')
+  const sliderWrapper = document.querySelector('.slider-wrapper')
   const sliderItems = document.querySelectorAll('.slider-container .slide-item')
   const slideCount = sliderItems.length
   let windowWidth = window.innerWidth
@@ -51,9 +52,6 @@
 
   }
 
-  
-  
-
   const setActiveIdx = (num) => {
     switch(true){
       case (num < 0) : 
@@ -70,52 +68,43 @@
 
 
   const setSlidePosition = () => {
-    
     sliderItems.forEach( (item, idx) => {
+      item.style.width = `${windowWidth}px`
       switch(true){
 
         case idx === activeIdx : 
           item.classList.add('active')
           
           position = 0 - (windowWidth * idx)
-          item.style.transform = `translate(${position}px, 0%)`
+          sliderWrapper.style.transform = `translate(${position}px, 0%)`
           break;
 
         case idx < activeIdx : 
         case idx > activeIdx : 
           item.classList.remove('active')
-
-          position = 0 - windowWidth * activeIdx
-          item.style.transform = `translate(${position}px, 0%)`
           break;
-
+          
       }
 
     })
   }
 
-  
-  
-  
-
-  prevBtn.addEventListener('click', function(){
-    activeIdx -= 1
+  const moveInit = () => {
     setActiveIdx(activeIdx)
     bulletHandler()
     setSlidePosition()
+  }
+
+  prevBtn.addEventListener('click', function(){
+    activeIdx -= 1
+    moveInit()
   })
 
   nextBtn.addEventListener('click', function(){
     activeIdx += 1
-    setActiveIdx(activeIdx)
-    bulletHandler()
-    setSlidePosition()
+    moveInit()
   })
 
-
-  
-  
-  
   window.addEventListener('resize', function(){
     windowWidth = window.innerWidth
     setSlidePosition()
@@ -124,6 +113,5 @@
   setBulletsTemplate()
   setSlidePosition()
   bulletHandler()
-
 
 })();
